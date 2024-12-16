26ab74f176ea904ab0c3bb872314d39a8740b200
morena1001 <josueflores15.jf@gmail.com>
2024-12-15 19:23:42 -0600

    new

--- Python version/main.py
+++ Python version/main.py
@@ -4,8 +4,16 @@ import mediapipe as mp
 vid = cv2.VideoCapture (0) # 1 for external webcam
 vid.set(3, 960)
 
+mphands = mp.solutions.hands
+Hands = mphands.Hands (max_num_hands = 1, min_detection_confidence = 0.7, min_tracking_confidence = 0.6)
+
 while True :
     success, frame = vid.read ()
 
+    # Convert bgr to rgb
+    RGBframe = cv2.cvtColor (frame, cv2.COLOR_BGR2RGB)
+    result = Hands.process (RGBframe)
+
+    # cv2.imshow ("rgbvideo", RGBframe)
     cv2.imshow ("video", frame)
-    cv2.waitKey
+    cv2.waitKey (1)
