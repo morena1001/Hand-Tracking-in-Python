08947c72c57c2253c4c0aa16b2fa3c0291169a71
morena1001 <josueflores15.jf@gmail.com>
2024-12-15 18:48:01 -0600

    Initial commit. Was going to do it in C++, but not much to work with

---
+++ C++ version/src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+// #include "opencv2/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main () {
+    
+    
+    
+    // VideoCapture cap (0);
+    // Mat frame;
+
+    // namedWindow ("Input", 0);
+    // namedWindow ("Output", 0);
+
+    // Ptr<BackgroundSubtractorKNN> knn = createBackgroundSubtractorKNN (1, 400, false);
+
+    // while (1) {
+    //     cap >> frame;
+    //     Mat outputFrame;
+    //     knn->apply (frame, outputFrame);
+
+    //     imshow ("Input", frame);
+    //     imshow ("Output", frame);
+
+    //     waitKey(1);
+    // }
+
+    return 0;
+}
---
+++ Python version/main.py
@@ -0,0 +1,11 @@
+import cv2
+import mediapipe
+
+vid = cv2.VideoCapture (0) # 1 for external webcam
+vid.set(3, 960)
+
+while True :
+    success, frame = vid.read ()
+
+    cv2.imshow ("video", frame)
+    cv2.waitKey
