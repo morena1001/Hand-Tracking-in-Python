e29b8ef152058caaff82d486e1a2e415d59ce74b
morena1001 <josueflores15.jf@gmail.com>
2024-12-15 21:42:02 -0600

    new

--- Python version/main.py
+++ Python version/main.py
@@ -4,6 +4,8 @@ import cv2
 import mediapipe as mp
 
 def distance (point1, point2) :
+    print (point1)
+    print (math.pow (point1[0] - point2[0], 2))
     return math.sqrt (math.pow (point1[0] - point2[0], 2) + (point1[1] - point2[1], 2))
 
 def is_fist (landmarks) :
