0e647ada2d35d869080a7e549873b712cd077e0b
morena1001 <josueflores15.jf@gmail.com>
2024-12-15 20:07:35 -0600

    new

--- Python version/main.py
+++ Python version/main.py
@@ -16,9 +16,9 @@ while True :
     result = Hands.process (RGBframe)
 
     cv2.circle (frame, (5, 5), 10, (0, 0, 255), cv2.FILLED)
-    cv2.circle (frame, (frame.shape[1] - 5, 5), 10, (0, 0, 255), cv2.FILLED)
-    cv2.circle (frame, (frame.shape[1] - 5, frame.shape[0] - 5), 10, (0, 0, 255), cv2.FILLED)
-    cv2.circle (frame, (5, frame.shape[0] - 5), 10, (0, 0, 255), cv2.FILLED)
+    cv2.circle (frame, (frame.shape[1] - 5, 5), 10, (0, 255, 255), cv2.FILLED)
+    cv2.circle (frame, (frame.shape[1] - 5, frame.shape[0] - 5), 10, (255, 0, 255), cv2.FILLED)
+    cv2.circle (frame, (5, frame.shape[0] - 5), 10, (255, 255, 0), cv2.FILLED)
     
     if result.multi_hand_landmarks :
         # print ("hand found")
