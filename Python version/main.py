cac4023dffeca6a1474d647f8156664836d1464c
morena1001 <josueflores15.jf@gmail.com>
2024-12-15 19:40:09 -0600

    new

--- Python version/main.py
+++ Python version/main.py
@@ -5,6 +5,7 @@ vid = cv2.VideoCapture (0) # 1 for external webcam
 vid.set(3, 960)
 
 mphands = mp.solutions.hands
+mpdraw = mp.solutions.drawing_utils
 Hands = mphands.Hands (max_num_hands = 1, min_detection_confidence = 0.7, min_tracking_confidence = 0.6)
 
 while True :
@@ -23,6 +24,7 @@ while True :
                 cx, cy = int (lm.x * w), int (lm.y * h)
                 print (id, cx, cy)
                 cv2.circle (frame, (cx, cy), 5, (0, 255, 0), cv2.FILLED)
+                mpdraw.draw_landmarks (frame, handLm, mphands.HAND_CONNECTIONS)
 
     # cv2.imshow ("rgbvideo", RGBframe)
     cv2.imshow ("video", frame)
